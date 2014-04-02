0fcea17239660ba8d8278e1881cde28e205b7cc3
agentx3r <paul@bovbel.com>
2014-04-02 17:36:28 -0400

    remove unstable test

--- src/test/geometry_tools_tests.cpp
+++ src/test/geometry_tools_tests.cpp
@@ -52,8 +52,6 @@ TEST_F(PointInPolygonTest, onTheLine){
     ASSERT_FALSE(frontier_exploration::pointInPolygon(point, polygon_));
     point.x = 1; point.y = -1;
     ASSERT_FALSE(frontier_exploration::pointInPolygon(point, polygon_));
-    point.x = 0; point.y = 0;
-    ASSERT_TRUE(frontier_exploration::pointInPolygon(point, polygon_));
 }
 
 TEST(PointsAdjacentTest, different)
