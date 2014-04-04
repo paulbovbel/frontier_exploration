dd7b15c7444c98b793f0e0c0adc787a7520ef1cd
agentx3r <paul@bovbel.com>
2014-04-04 16:15:49 -0400

    fix costmap_tools test

--- src/test/costmap_tools_tests.cpp
+++ src/test/costmap_tools_tests.cpp
@@ -9,17 +9,17 @@ class NeighborhoodFunctionTest : public::testing::Test{
 protected:
     virtual void SetUp(){
         //create map of 10 x 10
-        layer_.resizeMap(9,9,0.1,0,0);
+        costmap_.resizeMap(9,9,0.1,0,0);
     }
-    costmap_2d::Costmap2D layer_;
+    costmap_2d::Costmap2D costmap_;
 };
 
 TEST_F(NeighborhoodFunctionTest, middle)
 {
 
-    unsigned int idx = layer_.getIndex(4,4);
-    ASSERT_EQ(4,frontier_exploration::nhood4(idx,&layer_).size());
-    ASSERT_EQ(8,frontier_exploration::nhood8(idx,&layer_).size());
+    unsigned int idx = costmap_.getIndex(4,4);
+    ASSERT_EQ(4,frontier_exploration::nhood4(idx,costmap_).size());
+    ASSERT_EQ(8,frontier_exploration::nhood8(idx,costmap_).size());
 
 }
 
@@ -27,13 +27,13 @@ TEST_F(NeighborhoodFunctionTest, edge)
 {
 
     std::list<unsigned int> to_test;
-    to_test.push_back(layer_.getIndex(4,8));
-    to_test.push_back(layer_.getIndex(4,0));
-    to_test.push_back(layer_.getIndex(8,4));
-    to_test.push_back(layer_.getIndex(0,4));
+    to_test.push_back(costmap_.getIndex(4,8));
+    to_test.push_back(costmap_.getIndex(4,0));
+    to_test.push_back(costmap_.getIndex(8,4));
+    to_test.push_back(costmap_.getIndex(0,4));
     BOOST_FOREACH(unsigned int idx, to_test){
-        ASSERT_EQ(3,frontier_exploration::nhood4(idx,&layer_).size());
-        ASSERT_EQ(5,frontier_exploration::nhood8(idx,&layer_).size());
+        ASSERT_EQ(3,frontier_exploration::nhood4(idx,costmap_).size());
+        ASSERT_EQ(5,frontier_exploration::nhood8(idx,costmap_).size());
     }
 }
 
@@ -42,13 +42,13 @@ TEST_F(NeighborhoodFunctionTest, corner)
 {
 
     std::list<unsigned int> to_test;
-    to_test.push_back(layer_.getIndex(8,8));
-    to_test.push_back(layer_.getIndex(8,0));
-    to_test.push_back(layer_.getIndex(8,0));
-    to_test.push_back(layer_.getIndex(0,0));
+    to_test.push_back(costmap_.getIndex(8,8));
+    to_test.push_back(costmap_.getIndex(8,0));
+    to_test.push_back(costmap_.getIndex(8,0));
+    to_test.push_back(costmap_.getIndex(0,0));
     BOOST_FOREACH(unsigned int idx, to_test){
-        ASSERT_EQ(2,frontier_exploration::nhood4(idx,&layer_).size());
-        ASSERT_EQ(3,frontier_exploration::nhood8(idx,&layer_).size());
+        ASSERT_EQ(2,frontier_exploration::nhood4(idx,costmap_).size());
+        ASSERT_EQ(3,frontier_exploration::nhood8(idx,costmap_).size());
     }
 
 }
@@ -56,9 +56,9 @@ TEST_F(NeighborhoodFunctionTest, corner)
 TEST_F(NeighborhoodFunctionTest, offmap)
 {
 
-    unsigned int idx = layer_.getIndex(12,12);
-    ASSERT_EQ(0,frontier_exploration::nhood4(idx,&layer_).size());
-    ASSERT_EQ(0,frontier_exploration::nhood8(idx,&layer_).size());
+    unsigned int idx = costmap_.getIndex(12,12);
+    ASSERT_EQ(0,frontier_exploration::nhood4(idx,costmap_).size());
+    ASSERT_EQ(0,frontier_exploration::nhood8(idx,costmap_).size());
 
 }
 
