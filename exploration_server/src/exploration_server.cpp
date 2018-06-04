5843b69856000d866dd077a7b3d542251639cb63
Vicky McDermott <vmcdermott@locusrobotics.com>
2018-06-04 17:01:53 -0400

    starting off exploration server implementation

--- exploration_server/src/exploration_server.cpp
+++ exploration_server/src/exploration_server.cpp
@@ -1,6 +1,10 @@
 #include "exploration_server/exploration_server.h"
 
 #include <ros/ros.h>
+#include <actionlib/server/simple_action_server.h>
+#include <actionlib/client/simple_action_client.h>
+
+#include <move_base_msgs/MoveBaseAction.h>
 
 namespace exploration_server{
 
@@ -18,24 +22,28 @@ ExplorationServer::ExplorationServer(ros::NodeHandle nh, ros::NodeHandle private
 
 }
 
-// exploreGoalCb(goal_handle)
-//   1. preempt active goal handle, if any
-//   2. set as active goal handle
-//   3. initialize exploration planner plugin
-//   4. update boundary polygon on costmap, if necessary
-//   5. request next goal from planner pluigin
-//   6. send goal to move_base
-//
-// moveBaseResultCb(result)
-//   1. if successful,
-//     a) request next goal from planner pluginlib
-//   2. if failed
-//     a) 'handle' (retry, add to exploration blacklist PR#25, etc?)
-//     b) move on to next exploration target
-//     c) fail exploration?
-//
-// cancelGoalCb(goal_handle)
-//   1. if active goal handle, cancel
+void ExplorationServer::goalCB(const exploration_server::ExplorationTaskGoalConstPtr &goal){
+  //   1. preempt active goal handle, if any
+  //   2. set as active goal handle
+  //   3. initialize exploration planner plugin
+  //   4. update boundary polygon on costmap, if necessary
+  //   5. request next goal from planner pluigin
+  //   6. send goal to move_base
+}
+
+void ExplorationServer::moveBaseResultCb(const actionlib::SimpleClientGoalState& state, const move_base_msgs::MoveBaseResultConstPtr& result){
+  //   1. if successful,
+  //     a) request next goal from planner pluginlib
+  //   2. if failed
+  //     a) 'handle' (retry, add to exploration blacklist PR#25, etc?)
+  //     b) move on to next exploration target
+  //     c) fail exploration?
+}
+
+void ExplorationServer::cancelGoalCb(const exploration_server::ExplorationTaskGoalConstPtr &goal){
+  //   1. if active goal handle, cancel
+}
+
 
 
 }
